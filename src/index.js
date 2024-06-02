eaa913d5407b2a40fa185ec9c9ebcc72342c3c37
Sixuan Wu <sixuan@sixuans-imac.home>
2024-06-01 22:24:12 -0400

    fix Application Error: This application failed to respond in railway

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = 3001;
-app.listen(port, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
